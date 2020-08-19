56578f9499fa2641f3449d4e515375d04db7d6b6
ai73a@yandex.ru <1qazxcvBG>
2020-08-19 14:22:53 +0300

    fix import

--- dist/cnb-2-features/f-1-auth/a-3-helpers/h-2-more/errorStatuses.js
+++ dist/cnb-2-features/f-1-auth/a-3-helpers/h-2-more/errorStatuses.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.status400 = exports.status500 = void 0;
 const config_1 = require("../../../../cnb-1-main/config");
-const app_1 = require("../../../../cnb-1-main/app");
+const cookie_1 = require("../../../../cnb-1-main/cookie");
 exports.status500 = (res, e, user, inTry) => {
     const error = {
         error: "some error: " + e.message,
@@ -11,7 +11,7 @@ exports.status500 = (res, e, user, inTry) => {
         info: "Back doesn't know what the error is... ^._.^",
     };
     console.log("error-nya-500: ", error);
-    res.cookie("token", user.token, Object.assign(Object.assign({}, app_1.cookieSettings), { expires: new Date(user.tokenDeathTime || 0) })).status(500).json(error);
+    res.cookie("token", user.token, Object.assign(Object.assign({}, cookie_1.cookieSettings), { expires: new Date(user.tokenDeathTime || 0) })).status(500).json(error);
 };
 exports.status400 = (res, e, user, inTry) => {
     const error = {
@@ -20,6 +20,6 @@ exports.status400 = (res, e, user, inTry) => {
         info: "Check your request! /ᐠ-ꞈ-ᐟ\\",
     };
     console.log("error-nya-400: ", error);
-    res.cookie("token", user.token, Object.assign(Object.assign({}, app_1.cookieSettings), { expires: new Date(user.tokenDeathTime || 0) })).status(400).json(error);
+    res.cookie("token", user.token, Object.assign(Object.assign({}, cookie_1.cookieSettings), { expires: new Date(user.tokenDeathTime || 0) })).status(400).json(error);
 };
 //# sourceMappingURL=errorStatuses.js.map

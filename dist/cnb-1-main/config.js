d0e44f52075840d5226670e942dbec8cf72534b0
ai73a@yandex.ru <1qazxcvBG>
2020-08-19 19:34:57 +0300

    setPass final

--- dist/cnb-1-main/config.js
+++ dist/cnb-1-main/config.js
@@ -5,7 +5,7 @@ const USER_NAME = process.env.MONGO_DB_USER_NAME || "ai73aaa";
 const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1qazxcvBG";
 const MONGO_DB_URL = process.env.MONGO_DB_URL || "neko0-iwojt.mongodb.net/nekobd"; // bd for tests
 exports.MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
-exports.DEV_VERSION = true;
+exports.DEV_VERSION = false;
 exports.VERSION_2_0 = "/2.0";
 exports.GMAIL_USER = "";
 exports.GMAIL_PASS = "";

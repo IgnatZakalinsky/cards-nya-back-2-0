fa4debc733f6c712288be85c9f79e8bf7c02c046
IgnatZakalinsky <ai73a@yandex.ru>
2020-09-01 15:26:21 +0300

    add update grade

--- src/cnb-2-features/f-2-cards/c-1-controllers/grade/updateGrade.ts
+++ src/cnb-2-features/f-2-cards/c-1-controllers/grade/updateGrade.ts
@@ -1,25 +1,26 @@
 import {Request, Response} from "express";
 import {IUser} from "../../../f-1-auth/a-2-models/user";
-import {status400, status500} from "../../../f-1-auth/a-3-helpers/h-2-more/findUserByToken";
 import Card, {ICard} from "../../c-2-models/card";
 import Grade, {IGrade} from "../../c-2-models/grade";
+import {status400, status500} from "../../../f-1-auth/a-3-helpers/h-2-more/errorStatuses";
+import {resCookie} from "../../../../cnb-1-main/cookie";
 
 export const updateGrade = async (req: Request, res: Response, user: IUser) => {
     const {grade, card_id} = req.body;
 
-    if (!grade) status400(res, `No grade in body!`, user, 'updateGrade');
-    else if (!card_id) status400(res, `No Card id in body!`, user, 'updateGrade');
+    if (!grade) status400(res, "No grade in body! /ᐠ-ꞈ-ᐟ\\", user, "updateGrade");
+    else if (!card_id) status400(res, "No Card id in body! /ᐠ-ꞈ-ᐟ\\", user, "updateGrade");
 
     else {
         const gradeF = isFinite(grade) ? +grade : undefined;
 
         if (!gradeF || (gradeF && (gradeF > 5 || gradeF < 0))) status400(res,
-            `Grade [${gradeF}] not valid! must be between 0 and 5...`, user, 'updateGrade');
+            `Grade [${gradeF}] not valid! must be between 0 and 5... /ᐠ-ꞈ-ᐟ\\`, user, "updateGrade");
 
         else Card.findById(card_id)
             .exec()
             .then((oldCard: ICard | null) => {
-                if (!oldCard) status400(res, `Card id not valid`, user, 'updateGrade');
+                if (!oldCard) status400(res, "Card id not valid /ᐠ-ꞈ-ᐟ\\", user, "updateGrade");
 
                 else {
                     Grade.findOne({user_id: user._id, card_id})
@@ -32,15 +33,22 @@ export const updateGrade = async (req: Request, res: Response, user: IUser) => {
 
                                     grade: gradeF,
                                     shots: 1,
+
+                                    more_id: user._id,
+
+                                    created: new Date(),
+                                    updated: new Date(),
+
+                                    _doc: {},
                                 })
                                     .then((newGrade: IGrade) => {
-                                        res.status(201).json({
+                                        resCookie(res, user).status(201).json({
                                             updatedGrade: newGrade,
                                             token: user.token,
                                             tokenDeathTime: user.tokenDeathTime
                                         })
                                     })
-                                    .catch(e => status500(res, e, user, 'updateGrade/Grade.create'))
+                                    .catch(e => status500(res, e, user, "updateGrade/Grade.create"))
                             } else {
                                 const newShotsF = oldGrade.shots + 1;
                                 const newGradeF = ((oldGrade.grade * oldGrade.shots) + gradeF) / newShotsF;
@@ -53,20 +61,21 @@ export const updateGrade = async (req: Request, res: Response, user: IUser) => {
                                 )
                                     .exec()
                                     .then((updatedGrade: IGrade | null) => {
-                                        if (!updatedGrade) status400(res, `never`, user, 'updateGrade');
+                                        if (!updatedGrade)
+                                            status400(res, "not updated? /ᐠ｡ꞈ｡ᐟ\\", user, "updateGrade");
 
-                                        else res.status(200).json({
+                                        else resCookie(res, user).status(200).json({
                                             updatedGrade,
                                             token: user.token,
                                             tokenDeathTime: user.tokenDeathTime
-                                        })
+                                        });
                                     })
-                                    .catch(e => status500(res, e, user, 'updateCard/Card.findByIdAndUpdate'))
+                                    .catch(e => status500(res, e, user, "updateCard/Card.findByIdAndUpdate"));
                             }
                         })
-                        .catch(e => status500(res, e, user, 'updateGrade/Grade.findOne'));
+                        .catch(e => status500(res, e, user, "updateGrade/Grade.findOne"));
                 }
             })
-            .catch(e => status500(res, e, user, 'updateGrade/Card.findById'));
+            .catch(e => status500(res, e, user, "updateGrade/Card.findById"));
     }
 };

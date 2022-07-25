65feb85b3028d8e007c066eb431dfa8c23631ff5
neko <IgnatZakalinsky>
2022-07-25 11:20:53 +0300

    back to gmail

--- src/cnb-2-features/f-1-auth/a-3-helpers/h-3-gmail/gmail.ts
+++ src/cnb-2-features/f-1-auth/a-3-helpers/h-3-gmail/gmail.ts
@@ -1,27 +1,27 @@
 import nodeMailer from "nodemailer";
 import {DEV_VERSION, GMAIL_PASS, GMAIL_USER} from "../../../../cnb-1-main/config";
 
-// @ts-ignore
-const transporter = nodeMailer.createTransport({ service: "Outlook365",
-    host: "smtp.office365.com",
-    port: "587",
-    tls: {
-        ciphers: "SSLv3",
-        rejectUnauthorized: false,
-    },
-    auth: {
-        user: 'neko.cafe@outlook.com',
-        pass: process.env.GMAIL_PASS || GMAIL_PASS
-    }
-});
-// const transporter = nodeMailer.createTransport({
-//     service: "gmail",
+// // @ts-ignore
+// const transporter = nodeMailer.createTransport({ service: "Outlook365",
+//     host: "smtp.office365.com",
+//     port: "587",
+//     tls: {
+//         ciphers: "SSLv3",
+//         rejectUnauthorized: false,
+//     },
 //     auth: {
-//         type: "login",
-//         user: process.env.GMAIL_USER || GMAIL_USER,
+//         user: 'neko.cafe@outlook.com',
 //         pass: process.env.GMAIL_PASS || GMAIL_PASS
 //     }
 // });
+const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+        // type: "login",
+        user: process.env.GMAIL_USER || GMAIL_USER,
+        pass: process.env.GMAIL_PASS || GMAIL_PASS
+    }
+});
 
 export const sendMail = async (from: string, to: string, subject: string, html?: string, text?: string) => {
 

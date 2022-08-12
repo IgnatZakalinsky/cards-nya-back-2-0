93b1a153fb1338857449ffad6fd8646fdd484ac1
neko <ai73a@yandex.ru>
2022-08-12 12:29:39 +0300

    add pack props

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -37,15 +28,9 @@ mongoose_1.default.connect(config_1.MongoDBUris, {
     .then(() => {
     console.log("Nya-MongoDB connected successfully");
     const port = process.env.PORT || config_1.PORT;
-    server.listen(port, () => __awaiter(void 0, void 0, void 0, function* () {
+    server.listen(port, () => {
         console.log("Cards-Nya-back 2.0 listening on port: " + port);
-        // try {
-        //     const x = await sendMail('neko.cafe@outlook.com', 'ai73a@yandex.com', 'aaa', '<div style="background-color: lime">zzz</div>')
-        //     console.log(x)
-        // } catch (e) {
-        //     console.log(e, {...e})
-        // }
-    }));
+    });
 })
     .catch(e => console.log("Nya-MongoDB connection error: ", Object.assign({}, e)));
 process.on("unhandledRejection", (reason, p) => {

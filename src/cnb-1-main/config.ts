2d3ff61a890e79369f5b8892248f1a98bbf53a86
neko <ai73a@yandex.ru>
2022-09-12 19:07:39 +0300

    new public db

--- src/cnb-1-main/config.ts
+++ src/cnb-1-main/config.ts
@@ -1,6 +1,6 @@
-const USER_NAME = process.env.MONGO_DB_USER_NAME || "ai73aaa";
-const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1qazxcvBG";
-const MONGO_DB_URL = process.env.MONGO_DB_URL || "neko0-iwojt.mongodb.net/nekobd"; // bd for tests
+const USER_NAME = process.env.MONGO_DB_USER_NAME || "lesson5";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1qaz2wsx";
+const MONGO_DB_URL = process.env.MONGO_DB_URL || "cluster0.gbcdgvc.mongodb.net/nekobd"; // bd for tests
 
 export const MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
 
